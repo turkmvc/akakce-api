aa711fbdede5728e8929e79b7c6ba8f053b8230b
Cuma KÖSE <25532765+turkmvc@users.noreply.github.com>
2021-01-31 17:24:17 +0300

    Create Akakce.php

---
+++ src/Facades/Akakce.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace turkmvc\Akakce\Facades;
+use Illuminate\Support\Facades\Facade;
+
+class Akakce extends Facade{
+
+  protected static function getFacadeAccessor() { 
+
+  	return 'akakce'; 
+
+  } 
+}
+?>
